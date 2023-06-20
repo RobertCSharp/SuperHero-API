bbb4c3262149947167fda0d19d1bbe5796463cf0
Robert Stefan <rpahonie@yahoo.com>
2023-06-20 21:02:06 +0200

    Add project files.

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
